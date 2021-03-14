61f39b1796bcc71835012f6997422ab77cf476da
Dmitry Tsyplov <d.tsyplov@mail.ru>
2021-03-14 18:23:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Stars from './Stars'  
+
+
 
 function App() {
+
+    const count = [1,2,3,4,5];
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      
+        <Stars count={count} />
+        
     </div>
   );
 }
---
+++ src/Stars.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Star from './Star'
+// import {useState} from 'react';
+
+function Stars({count}) {
+    
+    
+
+    return (
+        <div>
+            <ul className="card-body-stars u-clearfix">
+
+    
+     {count.map((item)=> <li key={item} className={count.length < 1 && count.length > 5 ? 'none' : 'star_list'}> <Star /> </li> )}
+     
+     </ul>
+        </div>
+        
+    )
+    
+}
+
+
+
+export default Stars
+
+// star_list
